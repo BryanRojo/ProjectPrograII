3023d2a0f506669c4eface874adc114f9633f194
Bryan Castro <bryancastro1010@gamil.com>
2022-07-17 20:19:06 -0600

    Sube carpetas

---
+++ DotWarsProjectServer/src/main/java/ac/ucr/c01880/dotwarsprojectserver/DotWarsProjectServer.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ac.ucr.c01880.dotwarsprojectserver;
+
+/**
+ *
+ * @author bryan
+ */
+public class DotWarsProjectServer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
